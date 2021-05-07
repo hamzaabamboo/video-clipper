efacce706bb28251b121d46d86bd31b24a8f6fc8
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2021-05-08 04:13:20 +0700

    :wrench:

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ module.exports = {
     webpack5: true,
   },
   webpack: (config, { isServer, dev, webpack }) => {
+    config.output.chunkFilename = isServer
+      ? `${dev ? "[name]" : "[name].[fullhash]"}.js`
+      : `static/chunks/${dev ? "[name]" : "[name].[fullhash]"}.js`;
+
     config.output.hotUpdateMainFilename =
       "static/webpack/[fullhash].[runtime].hot-update.json";
 
